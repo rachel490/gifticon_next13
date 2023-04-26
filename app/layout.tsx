32e1b92d0ec60cd441c28c293944823026e11bd2
rachel490 <ccori12@naver.com>
2023-04-27 07:25:24 +0900

    :sparkles: setup styled-components in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import StyledComponentsRegistry from "@/lib/registry";
 
 export const metadata = {
   title: "⭐️Gifti",
@@ -11,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
