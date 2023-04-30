3f1e4929dffc49569955d96a1549823305565358
rachel490 <ccori12@naver.com>
2023-04-30 21:33:00 +0900

    :sparkles: add Text component
    props added : size, color, weight, children(text), as

---
+++ components/Text/Text.tsx
@@ -0,0 +1,27 @@
+import * as S from "./Text.styled";
+
+interface IProps {
+  size?: number;
+  color?: string;
+  weight?: "regular" | "medium" | "semiBold";
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+  children: string;
+  className?: string;
+}
+
+function Text({
+  size = 14,
+  color = "#000",
+  weight = "medium",
+  as = "span",
+  children,
+  className,
+}: IProps) {
+  return (
+    <S.CustomText size={size} weight={weight} color={color} as={as} className={className}>
+      {children}
+    </S.CustomText>
+  );
+}
+
+export default Text;
