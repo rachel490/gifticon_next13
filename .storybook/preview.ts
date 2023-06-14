6e6644cd287e7271072dc1d124db84ca395b81d7
rachel490 <ccori12@naver.com>
2023-06-14 10:14:53 +0900

    :bug: Fix Storybook issue having no access to theme & global style
    install addon & setup decorators for styled-component's global style & theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,8 @@
 import type { Preview } from "@storybook/react";
+import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/global";
+import theme from "../styles/theme";
 
 const preview: Preview = {
   parameters: {
@@ -16,3 +20,15 @@ const preview: Preview = {
 };
 
 export default preview;
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles: GlobalStyle,
+    Provider: ThemeProvider,
+    themes: {
+      light: theme,
+      dark: theme,
+    },
+    defaultTheme: "light",
+  }),
+];
