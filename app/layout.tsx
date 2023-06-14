35fd7f761e0175281db8fd62d3a657b67a4ce870
rachel490 <ccori12@naver.com>
2023-06-14 21:54:58 +0900

    :bug: Fix Storybook font issue
    change variable to className

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={appleSDGothicNeo.variable}>
+    <html lang="en" className={appleSDGothicNeo.className}>
       <body>
         <ReduxToolkitProvider>
           <StyledComponentsRegistry>
