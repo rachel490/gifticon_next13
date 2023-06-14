110fd041c0691ea19b82d9dfee7a28aefd409fde
Rachel Shin (dwell) <72786354+rachel490@users.noreply.github.com>
2023-06-14 20:35:55 +0900

    Feat: Card Story 추가  (#13)
    * :recycle: Seperate data & UI from Card component
    
    remove types object, put imageSize & padding variants in styled-components
    
    * :wrench: Add eslint rule
    
    no-use-before-define rule
    
    * :sparkles: Add Card story
    
    * :recycle: Change Card story default background to dark

---
+++ components/Card/Card.stories.ts
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { lastSaleListMockData, menuItemMockData, menuListMockData } from "./Card.mock";
+import Card from "./Card";
+
+const meta: Meta<typeof Card> = {
+  title: "Card",
+  component: Card,
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+  argTypes: {
+    type: {
+      control: false,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const MenuListItemCard: Story = {
+  args: {
+    type: "menuListItem",
+    data: menuListMockData[0],
+  },
+};
+
+export const LastSaleItemCard: Story = {
+  args: {
+    type: "lastSaleItem",
+    data: lastSaleListMockData[0],
+  },
+};
+
+export const MenuDetailItemCard: Story = {
+  args: {
+    type: "menuDetailItem",
+    data: menuItemMockData,
+  },
+};
