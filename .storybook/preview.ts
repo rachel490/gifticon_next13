385d6a10bb672f583056a7ce103ac777544f26fa
Rachel Shin (dwell) <72786354+rachel490@users.noreply.github.com>
2023-06-14 11:49:15 +0900

    Feat: Navbar Story 추가 (#11)
    * :truck: Change page route address
    
    use plural, change name - menu -> items / faq -> contacts
    
    * :recycle: Modify base routes constant to updated page routes
    
    comment out dynamic titles for Navbar
    
    * :lipstick: Modify tags for Navbar
    
    * :bug: Add baseUrl for Storybook support to path alias in Next.js
    
    * :bug: Fix Storybook issue having no access to theme & global style
    
    install addon & setup decorators for styled-component's global style & theme
    
    * :recycle: Change navbar title to empty string in ItemDetails page
    
    * :wrench: Modify Storybook default dark background color
    
    * :sparkles: Add Navbar story

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,14 @@
 import type { Preview } from "@storybook/react";
+import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/global";
+import theme from "../styles/theme";
 
 const preview: Preview = {
   parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
@@ -9,7 +16,22 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      values: [{ name: "dark", value: "#202020" }],
+    },
   },
 };
 
 export default preview;
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles: GlobalStyle,
+    Provider: ThemeProvider,
+    themes: {
+      light: theme,
+      dark: theme,
+    },
+    defaultTheme: "light",
+  }),
+];
