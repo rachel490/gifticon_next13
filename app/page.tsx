c49d058748406726c3fe4355696dbb0f0dd01889
rachel490 <ccori12@naver.com>
2023-06-19 08:00:15 +0900

    :lipstick: Add title and style to LastSaleItemList

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import styled, { useTheme } from "styled-components";
+import { useGetCategoryList, useGetLastSaleItemList } from "@/hooks/queries/gifti";
+import { Text } from "@/components/Text";
 import Card from "@/components/Card/Card";
 import Grid from "@/components/Grid/Grid";
-import { useGetCategoryList, useGetLastSaleItemList } from "@/hooks/queries/gifti";
 
 function HomePage() {
+  const { color } = useTheme();
+
   const {
     data: categoryListData,
     isLoading: isLoadingCategoryListData,
@@ -22,13 +26,47 @@ function HomePage() {
   return (
     <div>
       <Grid data={categoryListData.conCategory1s} type="category" />
-      <div>
-        {lastSaleItemListData.conItems.map(item => (
-          <Card type="lastSaleItem" key={item.id} data={item} />
-        ))}
-      </div>
+      <Section>
+        <TextContainer>
+          <Text color={color.primary} size={13}>
+            놓치지 마세요
+          </Text>
+          <Text as="h3" size={16} weight="semiBold" color={color.neutral600}>
+            오늘의 땡처리콘!
+          </Text>
+        </TextContainer>
+        <CardList>
+          {lastSaleItemListData.conItems.map(item => (
+            <Card type="lastSaleItem" key={item.id} data={item} />
+          ))}
+        </CardList>
+      </Section>
     </div>
   );
 }
 
 export default HomePage;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-block: 1.2rem;
+  row-gap: 1.4rem;
+`;
+
+const TextContainer = styled.div`
+  margin-left: 1.7rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+
+  span {
+    line-height: 1.6rem;
+  }
+
+  h3 {
+    line-height: 1.9rem;
+  }
+`;
+
+const CardList = styled.div``;
