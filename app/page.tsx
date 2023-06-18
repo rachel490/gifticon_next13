ab7ecf68e41b133b3684b0d87babd7a873ceefbc
rachel490 <ccori12@naver.com>
2023-06-19 03:11:46 +0900

    :sparkles: Use categoryList query in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import CardList from "@/components/Card/CardList";
 import Grid from "@/components/Grid/Grid";
-import { brandMockData, categoryMockData } from "@/components/Grid/Grid.mock";
+import { brandMockData } from "@/components/Grid/Grid.mock";
+import { useGetCategoryList } from "@/hooks/queries/gifti";
 
 function HomePage() {
+  const { data: categoryListData, isLoading, isError } = useGetCategoryList();
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error...</h1>;
+
   return (
     <div>
-      <Grid data={categoryMockData} type="category" />
+      <Grid data={categoryListData.conCategory1s} type="category" />
       <Grid data={brandMockData} type="brand" />
       <CardList />
     </div>
