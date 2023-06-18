8c92be85dc9f67375e0bdc59db7338ae3226ad61
rachel490 <ccori12@naver.com>
2023-06-19 07:24:11 +0900

    :sparkles: Add fetcher & query for lastSaleItem

--- api/fetchers.ts
+++ api/fetchers.ts
@@ -1,8 +1,13 @@
-import { ICategoryListResponse } from "@/types/api";
+import { ICategoryListResponse, ILastSaleItemResponse } from "@/types/api";
 import ncncAxios from "./core";
 import { API_URI } from "./uri";
 
 export const getCategoryList = async () => {
   const { data } = await ncncAxios.get<ICategoryListResponse>(API_URI.categoryList);
+  return data;
+};
+
+export const getLastSaleItemList = async () => {
+  const { data } = await ncncAxios.get<ILastSaleItemResponse>(API_URI.lastSaleList);
   return data;
 };
--- hooks/queries/gifti.ts
+++ hooks/queries/gifti.ts
@@ -1,11 +1,18 @@
 import { AxiosError } from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { getCategoryList } from "@/api/fetchers";
-import { ICategoryListResponse } from "@/types/api";
+import { getCategoryList, getLastSaleItemList } from "@/api/fetchers";
+import { ICategoryListResponse, ILastSaleItemResponse } from "@/types/api";
 
 export const useGetCategoryList = () => {
   return useQuery<ICategoryListResponse, AxiosError, ICategoryListResponse, string[]>({
     queryKey: ["category list"],
     queryFn: () => getCategoryList(),
+  });
+};
+
+export const useGetLastSaleItemList = () => {
+  return useQuery<ILastSaleItemResponse, AxiosError, ILastSaleItemResponse, string[]>({
+    queryKey: ["last sale item list"],
+    queryFn: () => getLastSaleItemList(),
   });
 };
