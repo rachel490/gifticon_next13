a80feb90fb260ebc96ad2dfe22a813411a6e6dc4
rachel490 <ccori12@naver.com>
2023-06-20 23:37:37 +0900

    :sparkles: Render Tab in FAQ page w/ dynamic data

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useGetFAQCategoryList } from "@/hooks/queries/gifti";
 import Tab from "@/components/Tab/Tab";
 
 function FAQPage() {
+  const { data: FAQCategoryListData, isLoading, isError } = useGetFAQCategoryList();
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error...</h1>;
+
   return (
     <div>
-      <Tab type="faq" />
+      <Tab type="faq" data={FAQCategoryListData.qaTypes} />
     </div>
   );
 }
