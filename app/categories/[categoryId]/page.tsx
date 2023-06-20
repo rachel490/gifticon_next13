44a66b1d498449b86dbe06d86d1c95c357aecee7
rachel490 <ccori12@naver.com>
2023-06-20 14:10:54 +0900

    :sparkles: Render Tab in Category page

--- app/categories/[categoryId]/page.tsx
+++ app/categories/[categoryId]/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import Tab from "@/components/Tab/Tab";
+import { useGetCategoryList } from "@/hooks/queries/gifti";
+
 function CategoryPage() {
-  return <div>CategoryPage</div>;
+  const {
+    data: categoryListData,
+    isLoading: isLoadingCategoryListData,
+    isError: isErrorCategoryListData,
+  } = useGetCategoryList();
+
+  if (isLoadingCategoryListData) return <h1>Loading...</h1>;
+  if (isErrorCategoryListData) return <h1>Error...</h1>;
+
+  return (
+    <div>
+      <Tab type="category" data={categoryListData.conCategory1s} />
+    </div>
+  );
 }
 
 export default CategoryPage;
