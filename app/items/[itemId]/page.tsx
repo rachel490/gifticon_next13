621be2f1491e9ceb2d054df85331a9dc5d07cc42
rachel490 <ccori12@naver.com>
2023-06-21 00:31:28 +0900

    :sparkles: Render item card in Items Page

--- app/items/[itemId]/page.tsx
+++ app/items/[itemId]/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useGetItemDetails } from "@/hooks/queries/gifti";
+import Card from "@/components/Card/Card";
+
 function MenuPage() {
-  return <div>MenuPage</div>;
+  const params = useParams();
+
+  const { data: itemDetailsData, isLoading, isError } = useGetItemDetails(Number(params.itemId));
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error...</h1>;
+
+  return (
+    <div>
+      <Card type="menuDetailItem" data={itemDetailsData.conItem} />
+    </div>
+  );
 }
 
 export default MenuPage;
