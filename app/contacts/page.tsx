cdf9b3a65b03c99906990b700600c7a2903d0268
rachel490 <ccori12@naver.com>
2023-06-20 23:53:02 +0900

    :sparkles: Render FAQ question list

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 
-import { useGetFAQCategoryList } from "@/hooks/queries/gifti";
+import { useGetFAQCategoryList, useGetFAQList } from "@/hooks/queries/gifti";
 import Tab from "@/components/Tab/Tab";
 
 function FAQPage() {
-  const { data: FAQCategoryListData, isLoading, isError } = useGetFAQCategoryList();
+  const {
+    data: FAQCategoryListData,
+    isLoading: isLoadingFAQCategoryListData,
+    isError: isErrorFAQCategoryListData,
+  } = useGetFAQCategoryList();
 
-  if (isLoading) return <h1>Loading...</h1>;
-  if (isError) return <h1>Error...</h1>;
+  const {
+    data: FAQListData,
+    isLoading: isLoadingFAQListData,
+    isError: isErrorFAQListData,
+  } = useGetFAQList(1);
+
+  if (isLoadingFAQCategoryListData || isLoadingFAQListData) return <h1>Loading...</h1>;
+  if (isErrorFAQCategoryListData || isErrorFAQListData) return <h1>Error...</h1>;
 
   return (
     <div>
       <Tab type="faq" data={FAQCategoryListData.qaTypes} />
+      <div>
+        {FAQListData.qas.map(item => (
+          <p key={item.id}>{item.question}</p>
+        ))}
+      </div>
     </div>
   );
 }
