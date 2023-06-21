1852cda79664013e49efc3053fd9186c4c202e0f
rachel490 <ccori12@naver.com>
2023-06-21 16:22:52 +0900

    :sparkles: Implement Button component

--- components/Button/button.styled.tsx
+++ components/Button/button.styled.tsx
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 
-export const StyledButton = styled.button`
+export const Button = styled.button`
+  width: 100%;
+  height: 8rem;
   background-color: ${({ theme }) => theme.color.primary};
-  width: 10rem;
-  height: 3rem;
+  color: ${({ theme }) => theme.color.neutral0};
+  font-size: ${({ theme }) => theme.fontSize[16]};
+  font-weight: 500;
+  line-height: 19.2px;
+  display: flex;
+  padding-top: 2.3rem;
+  justify-content: center;
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.color.neutral300};
+  }
 `;
